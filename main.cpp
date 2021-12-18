d23bc6d92a48187fdc8ae21b0833ce473b0f320f
Aleksandr Petrukhin <shmeleine@gmail.com>
2021-12-18 23:55:39 +0300

    [bench] add linear & barrier search

--- main.cpp
+++ main.cpp
@@ -36,6 +36,53 @@ static void BM_sumWithoutConversion(benchmark::State& state) {
 
 BENCHMARK(BM_sumWithoutConversion);
 
+constexpr uint64_t VEC_SIZE = 10000000;
+constexpr uint64_t TARGET = VEC_SIZE + 1;
+
+std::vector<uint64_t> initializeTest() {
+  std::vector<uint64_t> vec;
+  vec.reserve(VEC_SIZE + 1);
+  for (int i = 0; i < VEC_SIZE; ++i) {
+    vec.push_back(i);
+  }
+  return vec;
+}
+
+bool barrierSearch(const std::vector<uint64_t>& vec, uint64_t target) {
+  size_t i = 0;
+  for (; vec[i] != target; ++i) {
+  }
+  return i < vec.size();
+}
+
+static void BM_barrierSearch(benchmark::State& state) {
+  auto vec = initializeTest();
+  vec.push_back(TARGET);
+  for (auto _ : state) {
+    barrierSearch(vec, TARGET);
+  }
+}
+
+BENCHMARK(BM_barrierSearch);
+
+bool linearSearch(const std::vector<uint64_t>& vec, uint64_t target) {
+  for (int i = 0; i < vec.size(); ++i) {
+    if (vec[i] == target) {
+      return true;
+    }
+  }
+  return false;
+}
+
+static void BM_linearSearch(benchmark::State& state) {
+    auto vec = initializeTest();
+    for (auto _ : state) {
+        linearSearch(vec, TARGET);
+    }
+}
+
+BENCHMARK(BM_linearSearch);
+
 BENCHMARK_MAIN();
 
 // int main(int argc, char** argv) {
