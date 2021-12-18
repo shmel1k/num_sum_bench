6fd7adea7a8bddcb581e01b0be0db26a585cbef9
Aleksandr Petrukhin <shmeleine@gmail.com>
2021-12-18 23:56:58 +0300

    [misc] fix ident

--- main.cpp
+++ main.cpp
@@ -3,35 +3,35 @@
 #include "benchmark/include/benchmark/benchmark.h"
 
 int sumWithConversion(uint64_t n) {
-  std::string sumStr = std::to_string(n);
-  int sum = 0;
-  for (int i = sumStr.length() - 1; i >= 0; --i) {
-    sum += (sumStr.at(i) - '0');
-  }
-  return sum;
+    std::string sumStr = std::to_string(n);
+    int sum = 0;
+    for (int i = sumStr.length() - 1; i >= 0; --i) {
+        sum += (sumStr.at(i) - '0');
+    }
+    return sum;
 }
 
 static void BM_sumWithConversion(benchmark::State& state) {
-  for (auto _ : state) {
-    sumWithConversion(1844674407370955161);
-  }
+    for (auto _ : state) {
+        sumWithConversion(1844674407370955161);
+    }
 }
 
 BENCHMARK(BM_sumWithConversion);
 
 int sumWithoutConversion(uint64_t n) {
-  int sum = 0;
-  for (; n > 0;) {
-    sum += n % 10;
-    n = n / 10;
-  }
-  return sum;
+    int sum = 0;
+    for (; n > 0;) {
+        sum += n % 10;
+        n = n / 10;
+    }
+    return sum;
 }
 
 static void BM_sumWithoutConversion(benchmark::State& state) {
-  for (auto _ : state) {
-    sumWithoutConversion(1844674407370955161);
-  }
+    for (auto _ : state) {
+        sumWithoutConversion(1844674407370955161);
+    }
 }
 
 BENCHMARK(BM_sumWithoutConversion);
@@ -40,38 +40,38 @@ constexpr uint64_t VEC_SIZE = 10000000;
 constexpr uint64_t TARGET = VEC_SIZE + 1;
 
 std::vector<uint64_t> initializeTest() {
-  std::vector<uint64_t> vec;
-  vec.reserve(VEC_SIZE + 1);
-  for (int i = 0; i < VEC_SIZE; ++i) {
-    vec.push_back(i);
-  }
-  return vec;
+    std::vector<uint64_t> vec;
+    vec.reserve(VEC_SIZE + 1);
+    for (int i = 0; i < VEC_SIZE; ++i) {
+        vec.push_back(i);
+    }
+    return vec;
 }
 
 bool barrierSearch(const std::vector<uint64_t>& vec, uint64_t target) {
-  size_t i = 0;
-  for (; vec[i] != target; ++i) {
-  }
-  return i < vec.size();
+    size_t i = 0;
+    for (; vec[i] != target; ++i) {
+    }
+    return i < vec.size();
 }
 
 static void BM_barrierSearch(benchmark::State& state) {
-  auto vec = initializeTest();
-  vec.push_back(TARGET);
-  for (auto _ : state) {
-    barrierSearch(vec, TARGET);
-  }
+    auto vec = initializeTest();
+    vec.push_back(TARGET);
+    for (auto _ : state) {
+        barrierSearch(vec, TARGET);
+    }
 }
 
 BENCHMARK(BM_barrierSearch);
 
 bool linearSearch(const std::vector<uint64_t>& vec, uint64_t target) {
-  for (int i = 0; i < vec.size(); ++i) {
-    if (vec[i] == target) {
-      return true;
+    for (int i = 0; i < vec.size(); ++i) {
+        if (vec[i] == target) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 static void BM_linearSearch(benchmark::State& state) {
